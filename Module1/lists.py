79f86e733808d4e62c983de114a5ee6880ee0f9f
aldin <aldinihalimi@gmail.com>
2025-11-08 14:32:56 +0100

    lesson1

---
+++ Module1/lists.py
@@ -0,0 +1,27 @@
+to_do_list =["math homework","clean the room","research for history","fun"]
+
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2])
+print(to_do_list[3])
+
+shopping_list =["apples","bananas", 3,4.5]
+shopping_list.append("oranges")
+print(shopping_list)
+0
+
+shopping_list.insert(2,"lemons")
+
+print(shopping_list)
+
+to_do_list.remove("research for history")
+print(to_do_list)
+
+del to_do_list[1]
+print(to_do_list)
+
+to_do_list[0] = "physics homework"
+print(to_do_list)
+
+to_do_list[3] = 1
+print(to_do_list)
---
+++ Module1/variables.py
@@ -0,0 +1,28 @@
+name = "Aldin"
+print(name)
+
+temperature = 98.6
+print(temperature)
+print(type(temperature))
+
+
+
+age = 30
+print("Age is:",age)
+
+x = 5
+y = 123
+result = x + y
+print("Rezultati eshte:" ,result)
+
+age = age + 1
+
+print("age now is:",age)
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+
