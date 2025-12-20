47408a85aa8304d0f5854a46e8f3770eab6824b4
Student <aldinihalimi@gmail.com>
2025-12-20 14:33:07 +0100

    Lesson 4

---
+++ Module 7/user_inpit.py
@@ -0,0 +1,12 @@
+name = input("Enter Your Name;")
+print(f"Hello how are you,{name}")
+
+age = int(input("Enter Your Age:"))
+print(age)
+
+number1 = int(input("Enter  First Number:"))
+number2 = int(input("Enter Your Second Number:"))
+
+result = number1 + number2
+
+print(f"teh sum of {number1} and {number2} is {result}")
