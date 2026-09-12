8b5351d4557f338ed9811077d6eadb9c60bdbae2
aldini212 <aldinihalimi@gmail.com>
2026-09-12 12:31:40 +0200

    123
    12

---
+++ Module-21/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {"message": "Hello World!"}
+
+@app.get('/items')
+def read_items():
+    return {"items":["item 1" , "item 2" , "item 3" ]}
+
+
+@app.get ("/items/{item)id}")
+def read_item(item_id:int):
+    return{"item_id":item_id}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id, "name":"Test Name"}
+
+@app.get("/items/")
+
+@app.get("/items/")
+def get_items(skip:int = 0, limit: int =10):
+    return {"skip": skip, "limit":limit}
+
+@app.put("/items/{item_id}")
+def uptade_items(item_id: int, name:str, price: float):
+    return {"item_id":name ,"item_price": price}
+
+@app.delete("/items{item_id}")
+def delete_item(item_id:int):
+    return {"message": f"Item{item_id} deleted"}
